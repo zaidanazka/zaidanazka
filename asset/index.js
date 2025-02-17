49cb2df13ac24a5f244475fef8da8e451fc32dd4
zaidaqn009 <zaidanazkaalfariq2207@gmail.com>
2025-02-17 21:59:58 +0700

    change from bootstrap to vanilla

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,12 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+  const navbarToggler = document.getElementById('navbar-toggler');
+  const navbarNav = document.getElementById('navbarNav');
   const learnMoreButton = document.querySelector('.btn-primary');
   const contactUsButton = document.querySelector('.btn-outline-secondary');
+
+  navbarToggler.addEventListener('click', function() {
+    navbarNav.classList.toggle('show');
+  });
 
   learnMoreButton.addEventListener('click', function() {
     window.scrollTo({
